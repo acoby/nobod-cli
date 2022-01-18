1fc408d890bcc7dd9b830006cde02059dc9ed8c4
Thoralf Rickert-Wendt <trw@acoby.de>
2022-01-18 16:49:42 +0100

    v1.1.0

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This is a little Python container that can be used to run a NOBOD cli command. 
 
 A useful scenario is a CI/CD process to start a NOBOD Job.
+
+## Changelog
+
+### v1.1.0
+
+- better handling for reading loop for job finish states
+- added different cli arguments
+
+### v1.0.0
+
+- initial version with a small and simple client
+
--- nobod-cli.py
+++ nobod-cli.py
@@ -9,10 +9,11 @@ import logging
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-SCRIPT_VERSION = 'v1.0.0'
+SCRIPT_VERSION = 'v1.1.0'
 HTTP_TIMEOUT = 10
 HTTP_VERIFY_SSL = False
 SLEEP_TIME = 30
+MAX_WAIT_TIME = 600
 
 class RunCLI(object):
   # -----------------------------------------------------------------
@@ -20,7 +21,10 @@ class RunCLI(object):
   def __init__(self):
     # parse cmd arguments
     self.read_cli_args()
-    logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
+    if self.args.log_level == "DEBUG":
+      logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
+    else:
+      logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
 
     # create jobs
     tasks = self.nobod_ci_create_job()
@@ -38,32 +42,46 @@ class RunCLI(object):
     exitcode = 0
     for task in tasks.get('jobs'):
       jobId = task.get('job')
+      exitcode += self.nobod_ci_wait_for_job(jobId)
+
+    exit(exitcode)
+
+  # -----------------------------------------------------------------
+  # wait for jobs
+  def nobod_ci_wait_for_job(self, jobId):
+    loop = 0
+    exitcode = 0
+    while True:
+      if loop*SLEEP_TIME > self.args.job_finish_timeout:
+        logging.error('Job did not respond within {} seconds'.format(self.args.job_finish_timeout))
+        exitcode = 1
+        break
+
       job = self.nobod_ci_get_job(jobId)
       
       if job is None:
-        exitcode += 1
+        exitcode = 1
         logging.error('Job {} not found'.format(jobId))
-        continue
+        break
       
       finished = job.get('finished')
       if finished is not None:
-        returncode = job.get('job_returncode')
+        exitcode = job.get('job_returncode')
         result = job.get('job_result')
 
-        if returncode == 0:
+        if exitcode == 0:
           status = 'successful'
         else:
           status = 'failed'
         
         logging.info('Job {} finished with state {}.\nOutput:\n{}'.format(jobId, status, result))
+        break
 
-        exitcode += returncode
-        continue
+      logging.info('Waiting {} seconds ...'.format(self.args.job_wait_timeout))
+      time.sleep(self.args.job_wait_timeout)
+      loop += 1
 
-      logging.info('Waiting {} seconds ...'.format(SLEEP_TIME))
-      time.sleep(SLEEP_TIME)
-
-    exit(exitcode)
+    return exitcode
 
   # -----------------------------------------------------------------
   # create ci jobs
@@ -80,7 +98,7 @@ class RunCLI(object):
     logging.info('Calling: GET {}'.format(url))
 
     try:
-      result = requests.get(url, timeout=HTTP_TIMEOUT, auth=(username,password), headers=headers, verify=HTTP_VERIFY_SSL)
+      result = requests.get(url, timeout=self.args.http_timeout, auth=(username,password), headers=headers, verify=self.args.tls_verify)
       if result.status_code == 200:
         result.encoding = 'utf-8'
         return json.loads(result.text)
@@ -110,7 +128,7 @@ class RunCLI(object):
     logging.debug('Calling: POST {}'.format(url))
     
     try:
-      result = requests.post(url, timeout=HTTP_TIMEOUT, auth=(username,password), headers=headers, verify=HTTP_VERIFY_SSL)
+      result = requests.post(url, timeout=self.args.http_timeout, auth=(username,password), headers=headers, verify=self.args.tls_verify)
       if result.status_code == 200:
         result.encoding = 'utf-8'
         return json.loads(result.text)
@@ -127,10 +145,15 @@ class RunCLI(object):
   def read_cli_args(self):
     parser = argparse.ArgumentParser()
     parser.add_argument('--qualifier', action = 'store', type=str, help='qualifier for a service', required=True)
-    parser.add_argument('--instance', action = 'store', type=str, help='an optional concrete instance id', required=False)
+    parser.add_argument('--url', action = 'store', type=str, help='an URL to the NOBOD CI API', required=True)
     parser.add_argument('--username', action = 'store', type=str, help='the username to access the NOBOD CI API', required=True)
     parser.add_argument('--password', action = 'store', type=str, help='the password to access the NOBOD CI API', required=True)
-    parser.add_argument('--url', action = 'store', type=str, help='an URL to the NOBOD CI API', required=True)
+    parser.add_argument('--instance', action = 'store', type=str, help='an optional concrete instance id', required=False)
+    parser.add_argument('--job-wait-timeout', action = 'store', type=int, help='an optional concrete wait time in seconds', required=False, default=SLEEP_TIME)
+    parser.add_argument('--job-finish-timeout', action = 'store', type=int, help='wait for that time within the job should finish', required=False, default=MAX_WAIT_TIME)
+    parser.add_argument('--tls-verify', action = 'store', type=bool, help='check a valid TLS connection', required=False, default=HTTP_VERIFY_SSL)
+    parser.add_argument('--http-timeout', action = 'store', type=int, help='wait for that time for the API', required=False, default=HTTP_TIMEOUT)
+    parser.add_argument('--log-level', action = 'store', type=str, help='defines the log level', required=False, default="INFO", choices=["DEBUG","INFO"])
     self.args = parser.parse_args()
 
 # Run the tool
